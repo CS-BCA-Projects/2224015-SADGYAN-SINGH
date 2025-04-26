5ac45b7bd39532be63ca3ec09c3ce981db8cf5e7
ssadgyan <dsvvsadgyansinghindo@gmail.com>
2025-04-26 12:02:11 +0545

    Deleted files and folders from project

--- handyman pro/backend/generate-jwt-secret.js
+++
@@ -1,5 +0,0 @@
-const crypto = require("crypto");
-
-// Generate a 32-byte random string and convert it to hexadecimal
-const jwtSecret = crypto.randomBytes(32).toString("hex");
-console.log("Generated JWT_SECRET:", jwtSecret);
